43c6dd1917459553736ab3b6703ce8e5fff4d551
Zaven <zmnatz@gmail.com>
2022-12-06 11:48:47 -0500

    change to kilos

--- src/App.js
+++ src/App.js
@@ -8,45 +8,51 @@ import {
 import { useState, useMemo } from "react";
 import "./styles.css";
 
+// const WEIGHTS = [45, 35, 25, 10, 5, 2.5];
+const WEIGHTS = [25, 20, 15, 10, 5, 2.5, 1.25];
+
 export default function App() {
-  const [maxWeight, setMaxWeight] = useState(500);
-  const [bar, setBar] = useState(45);
-  const [with35, setWith35] = useState(true);
-  const [with55, setWith55] = useState(false);
-  const [with100, setWith100] = useState(false);
+  const [maxWeight, setMaxWeight] = useState(250);
+  const [bar, setBar] = useState(20);
+  // const [with35, setWith35] = useState(true);
+  // const [with55, setWith55] = useState(false);
+  // const [with100, setWith100] = useState(false);
   const map = useMemo(() => {
     const map = {};
     let i = bar;
     while (i < maxWeight) {
       const plates = {};
       let weight = i - bar;
-      if (with100) {
-        plates[100] = Number.parseInt(weight / 200, 10);
-        weight %= 200;
-      }
-      if (with55) {
-        plates["55"] = Number.parseInt(weight / 110, 10);
-        weight %= 110;
-      }
-      plates["45"] = Number.parseInt(weight / 90, 10);
-      weight %= 90;
-      if (with35) {
-        plates["35"] = Number.parseInt(weight / 70, 10);
-        weight %= 70;
-      }
-      plates["25"] = Number.parseInt(weight / 50, 10);
-      weight %= 50;
-      plates["10"] = Number.parseInt(weight / 20, 10);
-      weight %= 20;
-      plates["5"] = Number.parseInt(weight / 10, 10);
-      weight %= 10;
-      plates["2.5"] = Number.parseInt(weight / 5, 10);
+      // if (with100) {
+      //   plates[100] = Number.parseInt(weight / 200, 10);
+      //   weight %= 200;
+      // }
+      // if (with55) {
+      //   plates["55"] = Number.parseInt(weight / 110, 10);
+      //   weight %= 110;
+      // }
+      WEIGHTS.forEach((plate) => {
+        plates[plate + " "] = Number.parseInt((weight / plate) * 2, 10);
+        weight %= plate * 2;
+      });
+      // plates["45"] = Number.parseInt(weight / 90, 10);
+      // weight %= 90;
+      // if (with35) {
+      //   plates["35"] = Number.parseInt(weight / 70, 10);
+      //   weight %= 70;
+      // }
+      // plates["25"] = Number.parseInt(weight / 50, 10);
+      // weight %= 50;
+      // plates["10"] = Number.parseInt(weight / 20, 10);
+      // weight %= 20;
+      // plates["5"] = Number.parseInt(weight / 10, 10);
+      // weight %= 10;
+      // plates["2.5"] = Number.parseInt(weight / 5, 10);
       map[i] = plates;
-      i += 5;
+      i += WEIGHTS[WEIGHTS.length - 1] * 2;
     }
-    console.log(map);
     return map;
-  }, [bar, with35, with55, maxWeight, with100]);
+  }, [bar, maxWeight /*, with35, with55, with100*/]);
 
   return (
     <>
@@ -70,7 +76,7 @@ export default function App() {
           value={maxWeight}
           onChange={(e) => setMaxWeight(Number.parseInt(e.target.value, 10))}
         />
-        <label htmlFor="no35">No 35s</label>
+        {/* <label htmlFor="no35">No 35s</label>
         <input
           type="checkbox"
           id="no35"
@@ -90,7 +96,7 @@ export default function App() {
           id="with100"
           checked={with100}
           onChange={(e) => setWith100(e.target.checked)}
-        />
+        /> */}
       </div>
       <Table size="small">
         <TableHead>
